beaa8b8c5f253e12fe1f3836343d5868fefff2f4
koushikm2929 <koushikm2929@gmail.com>
2026-04-21 21:10:45 +0530

    debug server minimal

--- server/server.js
+++ server/server.js
@@ -1,32 +1,27 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import workspaceRouter from "./routes/workspaceRoutes.js";
-import projectRouter from "./routes/projectRoutes.js";
-import taskRouter from "./routes/taskRoutes.js";
-import commentRouter from "./routes/commentRoutes.js";
-import { protect } from './middlewares/authMiddleware.js';
-import { clerkMiddleware } from '@clerk/express';
-import { inngest, functions } from './inngest/index.js';
-import { serve } from "inngest/express";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware());
 
-app.get('/', (req, res) => res.send('Server is live!'));
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is live ✅");
+});
 
-// Webhooks
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// TEMP: comment everything else
 
-// Routes
-app.use("/api/workspaces", protect, workspaceRouter);
-app.use("/api/projects", protect, projectRouter);
-app.use("/api/tasks", protect, taskRouter);
-app.use("/api/comments", protect, commentRouter);
+// import { clerkMiddleware } from '@clerk/express';
+// app.use(clerkMiddleware());
 
-const PORT = process.env.PORT || 5000;
+// import { protect } from './middlewares/authMiddleware.js';
+// import workspaceRouter from "./routes/workspaceRoutes.js";
+// app.use("/api/workspaces", protect, workspaceRouter);
+
+// import { serve } from "inngest/express";
+// import { inngest, functions } from './inngest/index.js';
+// app.use("/api/inngest", serve({ client: inngest, functions }));
 
 export default app;
